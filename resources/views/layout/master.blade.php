debb11fb13a902c3699ca87708462dcdb05ae14e
sara <desara.kacani@w2020.eu>
2018-06-29 17:04:06 +0200

    add files to master

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -8,13 +8,23 @@
     <meta name="author" content="">
     <title>SB Admin - Start Bootstrap Template</title>
     <!-- Bootstrap core CSS-->
-    <link href="{{ asset('my/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
     <!-- Custom fonts for this template-->
-    <link href="{{ asset('my/vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('vendor/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css">
     <!-- Page level plugin CSS-->
-    <link href="{{ asset('my/vendor/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
+    <link href="{{ asset('vendor/datatables/dataTables.bootstrap4.css') }}" rel="stylesheet">
     <!-- Custom styles for this template-->
-    <link href="{{ asset('my/css/sb-admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/toastr.min.css') }}" rel="stylesheet">
+
+    @yield('stylesheet')
+    <script>
+        var APP_URL = '{{ url('/') }}';
+        @if(auth()->user()){
+            var logged_user = {{auth()->user()->id}};
+        }
+        @endif
+    </script>
 </head>
 
 <body class="fixed-nav sticky-footer bg-dark" id="page-top">
@@ -28,19 +38,21 @@
    @include('layout.footer')
 
     <!-- Bootstrap core JavaScript-->
-    <script src="{{ asset('my/vendor/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('my/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
     <!-- Core plugin JavaScript-->
-    <script src="{{ asset('my/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('vendor/jquery-easing/jquery.easing.min.js') }}"></script>
     <!-- Page level plugin JavaScript-->
-    <script src="{{ asset('my/vendor/chart.js/Chart.min.js') }}"></script>
-    <script src="{{ asset('my/vendor/datatables/jquery.dataTables.js') }}"></script>
-    <script src="{{ asset('my/vendor/datatables/dataTables.bootstrap4.js') }}"></script>
+    <script src="{{ asset('vendor/chart.js/Chart.min.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/jquery.dataTables.js') }}"></script>
+    <script src="{{ asset('vendor/datatables/dataTables.bootstrap4.js') }}"></script>
     <!-- Custom scripts for all pages-->
-    <script src="{{ asset('my/js/sb-admin.min.js') }}"></script>
     <!-- Custom scripts for this page-->
-    <script src="{{ asset('my/js/sb-admin-datatables.min.js') }}"></script>
-    <script src="{{ asset('my/js/sb-admin-charts.min.js') }}"></script>
+    <script src="{{asset('js/loading-overlay/src/loadingoverlay.js')}}"></script>
+    <script src="{{asset('js/toastr.min.js')}}"></script>
+    <script src="{{asset('js/myScripts.js')}}"></script>
+    <script src="{{asset('js/tasks/taskActions.js')}}"></script>
+    @yield('page-scripts')
 </div>
 </body>
 </html>
